4139ddc1af8b1edae4f3c965e2a50507d7f78a38
DexterDiocampo <143504490+DexterDiocampo@users.noreply.github.com>
2023-12-15 17:55:59 +0800

    Add files via upload

---
+++ checkout.js
@@ -0,0 +1,73 @@
+let cartItems = JSON.parse(sessionStorage.getItem('cartItems')) || [];
+
+document.addEventListener('DOMContentLoaded', function () {
+    updateCheckout();
+});
+
+function updateCheckout() {
+    const checkoutSection = document.getElementById('checkout-items');
+    const totalAmountSpan = document.getElementById('total-amount');
+
+    checkoutSection.innerHTML = '';
+
+    let totalAmount = 0;
+    cartItems.forEach(item => {
+        const itemDiv = document.createElement('div');
+        itemDiv.className = 'checkout-item';
+
+        const itemName = document.createElement('p');
+        itemName.textContent = item.name;
+        itemDiv.appendChild(itemName);
+
+        const itemPrice = document.createElement('p');
+        itemPrice.textContent = `Price: ₱${item.price.toFixed(2)}`;
+        itemDiv.appendChild(itemPrice);
+
+        checkoutSection.appendChild(itemDiv);
+        totalAmount += item.price;
+    });
+
+    totalAmountSpan.textContent = `Total Amount: ₱${totalAmount.toFixed(2)}`;
+
+    // Disable the buy button if the cart is empty
+    const buyNowButton = document.querySelector('.buy-now-btn');
+    buyNowButton.disabled = cartItems.length === 0;
+}
+
+function goBack() {
+    window.location.href = 'mainpage.html';
+}
+
+function buyNow() {
+    const buyNowButton = document.querySelector('.buy-now-btn');
+    
+    if (buyNowButton.disabled) {
+        // Do nothing if the button is disabled
+        return;
+    }
+
+    buyNowButton.textContent = 'Back';
+    buyNowButton.onclick = goBack;
+
+    const loadingScreen = document.getElementById('loading-screen');
+    loadingScreen.style.display = 'flex';
+
+    setTimeout(() => {
+        if (validateEmail()) {
+            setTimeout(() => {
+                loadingScreen.style.display = 'none';
+                const itemPurchased = document.getElementById('item-purchased');
+                itemPurchased.style.display = 'block';
+                itemPurchased.style.top = `${buyNowButton.offsetTop - 50}px`;
+            }, 2000);
+        } else {
+            loadingScreen.style.display = 'none';
+            buyNowButton.textContent = 'Buy Now';
+            buyNowButton.onclick = buyNow;
+        }
+    }, 2000);
+}
+
+function validateEmail() {
+    return true;
+}
